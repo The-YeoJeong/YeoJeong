a1563d1f02a133d534140294fbd5089129e4e2a0
jh0222 <gihyun0222@naver.com>
2022-07-22 17:20:32 +0900

    CommentDelete finish And CommentList progress

--- backend/yeojeong/src/main/java/com/project/yeojeong/dto/CommentFormDto.java
+++ backend/yeojeong/src/main/java/com/project/yeojeong/dto/CommentFormDto.java
@@ -3,11 +3,15 @@ package com.project.yeojeong.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class CommentFormDto {
     private int commentNo;
     private int postNo;
-    private int memberId;
+    private String memberId;
+    private String memberNickName;
     private String commentContent;
+    private LocalDateTime createdTime;
 }
--- backend/yeojeong/src/main/java/com/project/yeojeong/service/CommentService.java
+++ backend/yeojeong/src/main/java/com/project/yeojeong/service/CommentService.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -32,5 +34,21 @@ public class CommentService {
     public void commentEdit(int commentNo, CommentFormDto commentFormDto) {
         Comment comment = commentRepository.getReferenceById(commentNo);
         comment.updateComment(commentFormDto);
+    }
+
+    public List<CommentFormDto> commentlist(int postNo) {
+        Post post = postRepository.getReferenceById(postNo);
+        List<Comment> commentList = commentRepository.getByPost(post);
+        List<CommentFormDto> commentFormDtos = new ArrayList<>();
+
+        for (int i=0;i<commentList.size();i++){
+            commentFormDtos.get(i).setCommentNo(commentList.get(i).getCommentNo());
+            commentFormDtos.get(i).setMemberId(commentList.get(i).getMember().getMemberId());
+            commentFormDtos.get(i).setMemberNickName(commentList.get(i).getMember().getMemberNickname());
+            commentFormDtos.get(i).setCommentContent(commentList.get(i).getCommentContent());
+            commentFormDtos.get(i).setCreatedTime(commentList.get(i).getCreatedTime());
+        }
+
+        return commentFormDtos;
     }
 }
