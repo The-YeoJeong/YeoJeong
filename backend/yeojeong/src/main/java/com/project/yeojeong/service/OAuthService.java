6db4f8e91b72bd18f49642c80700681460f893f5
Covy-jinseok <pjpj1018@gsitm.com>
2022-07-25 15:53:58 +0900

    oauth google done + id, nick duplicate fix

--- backend/yeojeong/src/main/java/com/project/yeojeong/service/OAuthService.java
+++ backend/yeojeong/src/main/java/com/project/yeojeong/service/OAuthService.java
@@ -1,26 +1,34 @@
 package com.project.yeojeong.service;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 @Service
-public class OAuthService{
-    // 인가코드로 Access Token 발급
-    public String getKakaoAccessToken (String code) {
+public class OAuthService {
+    @Value("${oauth2.google.client_id}")
+    private String GOOGLE_CLIENT_ID;
+
+    @Value("${oauth2.google.client_secret}")
+    private String GOOGLE_CLIENT_SECRET;
+
+    @Value("${oauth2.naver.client_id}")
+    private String NAVER_CLIENT_ID;
+
+    @Value("${oauth2.naver.client_secret}")
+    private String NAVER_CLIENT_SECRET;
+
+    @Value("${oauth2.kakao.client_id}")
+    private String KAKAO_CLIENT_ID;
+
+    public String getGoogleAccessToken(String code) {
         String access_Token = "";
-        String refresh_Token = "";
-        String reqURL = "https://kauth.kakao.com/oauth/token";
+        String reqURL = "https://oauth2.googleapis.com/token";
 
         try {
             URL url = new URL(reqURL);
@@ -34,9 +42,10 @@ public class OAuthService{
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
             StringBuilder sb = new StringBuilder();
             sb.append("grant_type=authorization_code");
-            sb.append("&client_id=d173137e871d96ad298bf43551057b2a"); // TODO REST_API_KEY 입력
-            sb.append("&redirect_uri=http://localhost/oauth2/kakao/login"); // TODO 인가코드 받은 redirect_uri 입력
-            sb.append("&code=" + code);
+            sb.append("&client_id=").append(GOOGLE_CLIENT_ID);
+            sb.append("&client_secret=").append(GOOGLE_CLIENT_SECRET);
+            sb.append("&redirect_uri=http://localhost/oauth2/google/login");
+            sb.append("&code=").append(code);
             bw.write(sb.toString());
             bw.flush();
 
@@ -59,10 +68,107 @@ public class OAuthService{
             JsonElement element = parser.parse(result);
 
             access_Token = element.getAsJsonObject().get("access_token").getAsString();
-            refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
 
-            System.out.println("@access_token : " + access_Token);
-            System.out.println("refresh_token : " + refresh_Token);
+            System.out.println("google access_token : " + access_Token);
+
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return access_Token;
+    }
+
+    // Access Token으로 사용자 정보 조회
+    public String createGoogleUser(String token) {
+        String reqURL = "https://www.googleapis.com/oauth2/v2/userinfo";
+
+        String id = "";
+        //access_token을 이용하여 사용자 정보 조회
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("GET");
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Authorization", "Bearer " + token); //전송할 header 작성, access_token전송
+
+            //결과 코드가 200이라면 성공
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode : " + responseCode);
+
+            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("response body : " + result);
+
+            //Gson 라이브러리로 JSON파싱
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+            String str = element.getAsJsonObject().get("id").toString();
+
+            id = str.substring(1, str.length() - 1);
+
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "G" + id;
+    }
+
+
+    // 인가코드로 Access Token 발급
+    public String getKakaoAccessToken(String code) {
+        String access_Token = "";
+        String reqURL = "https://kauth.kakao.com/oauth/token";
+
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            //POST 요청을 위해 기본값이 false인 setDoOutput을 true로
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+            //POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            StringBuilder sb = new StringBuilder();
+            sb.append("grant_type=authorization_code");
+            sb.append("&client_id=").append(KAKAO_CLIENT_ID); // TODO REST_API_KEY 입력
+            sb.append("&redirect_uri=http://localhost/oauth2/kakao/login"); // TODO 인가코드 받은 redirect_uri 입력
+            sb.append("&code=").append(code);
+            bw.write(sb.toString());
+            bw.flush();
+
+            //결과 코드가 200이라면 성공
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode : " + responseCode);
+
+            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("response body : " + result);
+
+            //Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            access_Token = element.getAsJsonObject().get("access_token").getAsString();
+
+            System.out.println("access_token : " + access_Token);
 
             br.close();
             bw.close();
@@ -106,7 +212,7 @@ public class OAuthService{
             JsonElement element = parser.parse(result);
 
             id = element.getAsJsonObject().get("id").toString();
-;
+
             br.close();
 
         } catch (IOException e) {
@@ -116,9 +222,8 @@ public class OAuthService{
         return "K" + id;
     }
 
-    public String getNaverAccessToken (String code, String state) {
+    public String getNaverAccessToken(String code, String state) {
         String access_Token = "";
-        String refresh_Token = "";
         String reqURL = "https://nid.naver.com/oauth2.0/token";
 
         try {
@@ -133,10 +238,10 @@ public class OAuthService{
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
             StringBuilder sb = new StringBuilder();
             sb.append("grant_type=authorization_code");
-            sb.append("&client_id=KWUoslendQDxZRSrkOFh"); // TODO REST_API_KEY 입력
-            sb.append("&client_secret=fXcuXgPFjB"); // TODO REST_API_KEY 입력
-            sb.append("&code=" + code);
-            sb.append("&state=" + state);
+            sb.append("&client_id=").append(NAVER_CLIENT_ID);
+            sb.append("&client_secret=").append(NAVER_CLIENT_SECRET);
+            sb.append("&code=").append(code);
+            sb.append("&state=").append(state);
             bw.write(sb.toString());
             bw.flush();
 
@@ -159,10 +264,8 @@ public class OAuthService{
             JsonElement element = parser.parse(result);
 
             access_Token = element.getAsJsonObject().get("access_token").getAsString();
-            refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
 
             System.out.println("access_token : " + access_Token);
-            System.out.println("refresh_token : " + refresh_Token);
 
             br.close();
             bw.close();
@@ -199,6 +302,7 @@ public class OAuthService{
             while ((line = br.readLine()) != null) {
                 result += line;
             }
+
             System.out.println("response body : " + result);
 
             //Gson 라이브러리로 JSON파싱
@@ -206,10 +310,9 @@ public class OAuthService{
             JsonElement element = parser.parse(result);
             String str = element.getAsJsonObject().get("response").getAsJsonObject().get("id").toString();
 
-            id = "N" + str.substring(1, str.length() -1 );
+            id = "N" + str.substring(1, str.length() - 1);
 
             br.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
