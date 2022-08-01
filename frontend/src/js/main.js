6549911a5dd227c8b0d6429d08a1bccf9a45ddc6
DESKTOP-3ETIRCI\송진영 <songdh21@naver.com>
2022-08-01 22:10:27 +0900

    ✨feat: map -ing

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -7,8 +7,28 @@ const mainNode = () => {
   node.innerHTML = main;
   if (window.location.pathname !== '/') window.history.pushState(null, null, '/');
 
+  let regionName = '';
+  let period = '';
+  let postContent = '';
+  let order = '';
+  let searchContent = '';
+
   // Event
   postFunc.top3posts(node.querySelector('.top3-container'));
+
+  node.querySelector('.plan-city').addEventListener('click', e => {
+    e.target.style.backgroundColor = '#60b2ff';
+    regionName = e.target.textContent;
+    document.querySelector('#city-name').value = regionName;
+  });
+
+  node.querySelector('.period-buttons').addEventListener('click', e => {
+    if (e.target.tagName === 'BUTTON') {
+      e.target.style.border = '3px solid black';
+      period = e.target.dataset.id;
+      document.querySelector('#travel-period').value = e.target.textContent;
+    }
+  });
 
   return node.children;
 };
